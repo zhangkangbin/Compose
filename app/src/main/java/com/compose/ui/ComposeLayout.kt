f9e9b9414b42a4449c0f94d729688b04485ad02e
zhangkangbin <784908058@qq.com>
2023-03-16 18:29:27 +0800

    Compose Layout

--- app/src/main/java/com/compose/ui/ComposeLayout.kt
+++ app/src/main/java/com/compose/ui/ComposeLayout.kt
@@ -1,13 +1,18 @@
 package com.compose.ui
 
 import android.util.Log
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.magnifier
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -29,45 +34,80 @@ class ComposeLayout {
     @Composable
     fun body() {
 
-        Column {
-            SearchBar()
-            Row(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-            ) {
-                BodyElement()
-                BodyElement()
-                BodyElement()
-                BodyElement()
+        val numbers = mutableListOf(0,8,5,5,50,9,99,1,2,4)
+
+            Scaffold(
+                bottomBar = { SootheBottomNavigation() }
+            ) { padding ->
+                Column (   Modifier.verticalScroll(rememberScrollState())){
+                    SearchBar()
+                    LazyRow(
+                        modifier = Modifier
+                            .padding(8.dp)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+
+                        items(numbers){
+                            BodyElement()
+                        }
+
+
+                    }
+
+                    Row(
+                        modifier = Modifier
+                            .padding(top = 10.dp)
+                            .background(Color.Gray)
+                            .fillMaxWidth(),
+                        horizontalArrangement = Arrangement.Center,
+                    ) {
+
+                        Text(
+                            "AA",
+                            Modifier
+                                .padding(16.dp)
+                                .weight(1f), textAlign = TextAlign.Center
+                        )
+                        Text(
+                            "BB",
+                            Modifier
+                                .padding(16.dp)
+                                .weight(1f), textAlign = TextAlign.Center
+                        )
+                    }
+
+                    LazyHorizontalGrid(
+                        rows = GridCells.Fixed(2),
+                        modifier   = Modifier.height(200.dp)
+                    ) {
+                        items(numbers) { item ->
+                            CardListItem(item)
+                        }
+                    }
+
+                    HomeContent(title = "活动详情",Modifier.padding(10.dp)){
+
+                        CardListItem(1)
+                        CardListItem(2)
+                        CardListItem(2)
+                        CardListItem(2)
+                    }
+
+                    HomeContent(title = "商品列表",Modifier.padding(10.dp)){
+
+                        CardListItem(1)
+                        CardListItem(2)
+                        CardListItem(2)
+                        CardListItem(2)
+                        CardListItem(2)
+                        CardListItem(2)
+                    }
+                }
             }
 
-            Row(
-                modifier = Modifier
-                    .padding(top = 10.dp)
-                    .background(Color.Gray)
-                    .fillMaxWidth(),
-                horizontalArrangement = Arrangement.Center,
-            ) {
 
-                Text(
-                    "AA",
-                    Modifier
-                        .padding(16.dp)
-                        .weight(1f), textAlign = TextAlign.Center
-                )
-                Text(
-                    "BB",
-                    Modifier
-                        .padding(16.dp)
-                        .weight(1f), textAlign = TextAlign.Center
-                )
-            }
 
-            CardListItem()
-            CardListItem()
-        }
     }
 
     @Preview
@@ -120,15 +160,15 @@ class ComposeLayout {
 
     }
 
-    @Preview
+
     @Composable
-    fun CardListItem() {
+    fun CardListItem(index: Int) {
 
         Surface(shape = MaterialTheme.shapes.small, modifier = Modifier.padding(6.dp)) {
             Row(
                 Modifier
                     .width(192.dp)
-                    .height(56.dp)) {
+                    ) {
 
                 Image(
                     painter = painterResource(id = R.drawable.img),
@@ -137,8 +177,53 @@ class ComposeLayout {
                     modifier = Modifier.size(56.dp)
                 )
 
-                Text(text = "Nature..", Modifier.padding(6.dp))
+                Text(text = "Nature..$index", Modifier.padding(6.dp))
             }
+        }
+    }
+
+    /**
+     * 模块坑位。TopAppBar 为 title、navigationIcon 和 actions 提供槽位。
+     */
+    @Composable
+    fun HomeContent(title:String,modifier: Modifier=Modifier,content: @Composable () -> Unit){
+        Column(modifier) {
+            Text(text = title,    style = MaterialTheme.typography.h6,)
+            content()//模块坑位
+        }
+
+    }
+
+
+    @Composable
+    private fun SootheBottomNavigation(modifier: Modifier = Modifier) {
+        BottomNavigation(modifier,  backgroundColor = MaterialTheme.colors.background,) {
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.Add,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_home))
+                },
+                selected = true,
+                onClick = {}
+            )
+            BottomNavigationItem(
+                icon = {
+                    Icon(
+                        imageVector = Icons.Default.AccountCircle,
+                        contentDescription = null
+                    )
+                },
+                label = {
+                    Text(stringResource(R.string.bottom_navigation_profile))
+                },
+                selected = false,
+                onClick = {}
+            )
         }
     }
 }
